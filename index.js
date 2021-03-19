5fe0474e988187d85fff48ca519b04f66de38611
ShepLT1 <lshepherd234@gmail.com>
2021-03-19 16:46:11 -0700

    Added discord webhook notification system

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const { Webhook } = require('discord-webhook-node');
+const hook = new Webhook("https://discord.com/api/webhooks/822600815554330674/rEdI_ksH_BXLY4LmWnTTWMwiLRMsGJsSkefiDn6lvJ0Ie3Z-bMqrJ66usKMfXFSAyyVI");
 
 (async () => {
   const browser = await puppeteer.launch({ product: 'firefox' });
@@ -18,24 +20,33 @@ const puppeteer = require('puppeteer');
     await page.goto('https://www.bestbuy.com/site/hp-omen-30l-gaming-desktop-amd-ryzen-7-series-3700x-16gb-memory-nvidia-geforce-rtx-2060-1tb-hdd-256gb-ssd-black/6402514.p?skuId=6402514');
     await page.waitForSelector('.add-to-cart-button')
     const buttonText = await page.$eval('.add-to-cart-button', el => el.innerText);
-    console.log(buttonText)
     await page.waitForTimeout(5000)
 
     if (buttonText === "Add to Cart") {
-      // await page.evaluate(() =>
-      //   document.querySelectorAll('.add-to-cart-button')[0].scrollIntoView())
+      await page.evaluate(() =>
+        document.querySelectorAll('.add-to-cart-button')[0].scrollIntoView(false))
       await page.evaluate(() =>
         document.querySelectorAll(".add-to-cart-button")[0].click()
       );
+      await page.screenshot({ path: 'added-to-cart.png' }, { fullPage: true });
+      console.log("add to cart screenshot acquired")
+      hook.send("Added rtx 3070 to cart!");
+      hook.sendFile("./added-to-cart.png");
       await page.waitForSelector('.dot')
       await page.goto('https://www.bestbuy.com/cart');
       await page.waitForSelector('.checkout-buttons__checkout');
       await page.evaluate(() =>
         document.querySelectorAll('.checkout-buttons__checkout button:not(disabled)')[0].click()
       );
-      await page.waitForSelector('#fld-e');
-      await page.screenshot({ path: 'rtx3070.png' }, { fullPage: true });
-      console.log("screenshot acquired")
+      await page.screenshot({ path: 'checkout.png' }, { fullPage: true });
+      console.log("click checkout button screenshot acquired")
+      hook.send("Proceeding with checkout!");
+      hook.sendFile("./checkout.png");
+      await page.waitForSelector('.cia-form__controls');
+      await page.screenshot({ path: 'login-page.png' }, { fullPage: true });
+      console.log("log in screenshot acquired");
+      hook.send("Arrived at login page!");
+      hook.sendFile("./login-page.png");
     }
   } catch (e) {
     browser.close()
