82e070c48cb5174d6f74b16cdea21d2189ae0fa7
ShepLT1 <lshepherd234@gmail.com>
2021-03-21 16:14:11 -0700

    Added stealth mode plugin to avoid bot detection. Modified wait for selectors to avoid bot getting stuck. Added functionality to navigate to shipping info page, fill out information, then proceed to payment info page

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const pluginStealth = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(pluginStealth())
 const { Webhook } = require('discord-webhook-node');
 const hook = new Webhook("https://discord.com/api/webhooks/822600815554330674/rEdI_ksH_BXLY4LmWnTTWMwiLRMsGJsSkefiDn6lvJ0Ie3Z-bMqrJ66usKMfXFSAyyVI");
+const user = require('./config');
 
 const updateDiscord = async (page, message, file) => {
   try {
@@ -14,7 +17,11 @@ const updateDiscord = async (page, message, file) => {
 }
 
 (async () => {
-  const browser = await puppeteer.launch({ product: 'firefox' });
+  const browser = await puppeteer.launch({
+    ignoreHTTPSErrors: true,
+    headless: false,
+    product: 'firefox'
+  });
   const page = await browser.newPage();
   await page.emulate({
     name: "Desktop 10240x1200",
@@ -24,14 +31,15 @@ const updateDiscord = async (page, message, file) => {
     },
   });
 
-  // test: https://www.bestbuy.com/site/hp-omen-30l-gaming-desktop-amd-ryzen-7-series-3700x-16gb-memory-nvidia-geforce-rtx-2060-1tb-hdd-256gb-ssd-black/6402514.p?skuId=6402514
+  // test: https://www.bestbuy.com/site/pny-geforce-gt1030-2gb-pci-e-3-0-graphics-card-black/5901353.p?skuId=5901353
   // 3070: https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442
 
   try {
-    await page.goto('https://www.bestbuy.com/site/hp-omen-30l-gaming-desktop-amd-ryzen-7-series-3700x-16gb-memory-nvidia-geforce-rtx-2060-1tb-hdd-256gb-ssd-black/6402514.p?skuId=6402514');
-    await page.waitForSelector('.add-to-cart-button')
+    await page.goto('https://www.bestbuy.com/site/pny-geforce-gt1030-2gb-pci-e-3-0-graphics-card-black/5901353.p?skuId=5901353');
+    await page.waitForSelector('.blue-assist-tab', { timeout: 20000 })
     const buttonText = await page.$eval('.add-to-cart-button', el => el.innerText);
-    await page.waitForTimeout(5000)
+    // console.log(buttonText)
+    // await page.waitForTimeout(5000)
 
     if (buttonText === "Add to Cart") {
       await page.evaluate(() =>
@@ -40,15 +48,48 @@ const updateDiscord = async (page, message, file) => {
         document.querySelectorAll(".add-to-cart-button")[0].click()
       );
       await updateDiscord(page, 'Added rtx 3070 to cart!', 'added-to-cart.png');
-      await page.waitForSelector('.dot')
+      await page.waitForSelector('.dot', { timeout: 20000 })
       await page.goto('https://www.bestbuy.com/cart');
-      await page.waitForSelector('.checkout-buttons__checkout');
+      await page.waitForSelector('.checkout-buttons__checkout', { timeout: 20000 });
       await page.evaluate(() =>
         document.querySelectorAll('.checkout-buttons__checkout button:not(disabled)')[0].click()
       );
       await updateDiscord(page, 'Proceeding with checkout!', 'checkout.png');
-      await page.waitForSelector('.cdi-input');
-      await updateDiscord(page, 'Arrived at login page!', 'login-page.png');
+      await page.waitForSelector('.cia-guest-content', { timeout: 20000 });
+      await page.evaluate(() =>
+        document.querySelectorAll('.cia-guest-content__continue')[0].click()
+      );
+      await updateDiscord(page, 'Continuing as guest!', 'guest.png');
+      await page.waitForSelector('.streamlined__heading', { timeout: 20000 })
+      const headingText = await page.$eval('.streamlined__heading span', el => el.innerText);
+      if (headingText === "Store Pickup Information") {
+        console.log("store pickup")
+      } else {
+        await page.waitForSelector('.address-form__first-name', { timeout: 20000 })
+        await page.type('.address-form__first-name input', user.shipping.first, { delay: 100 })
+        await page.type('.address-form__last-name input', user.shipping.last, { delay: 100 })
+        await page.evaluate(() =>
+          document.querySelectorAll(".autocomplete__toggle")[0].click()
+        );
+        await page.type('[id="consolidatedAddresses.ui_address_2.street"]', user.shipping.street, { delay: 100 })
+        await page.evaluate(() =>
+          document.querySelectorAll(".address-form__showAddress2Link")[0].click()
+        );
+        await page.waitForSelector('[id="consolidatedAddresses.ui_address_2.street2"]', { timeout: 20000 })
+        await page.type('[id="consolidatedAddresses.ui_address_2.street2"]', user.shipping.street2, { delay: 100 })
+        await page.type('[id="consolidatedAddresses.ui_address_2.city"]', user.shipping.city, { delay: 100 })
+        await page.select('select[name="state"]', user.shipping.state)
+        await page.type('[id="consolidatedAddresses.ui_address_2.zipcode"]', user.shipping.zip, { delay: 100 })
+        await page.type('[id="user.emailAddress"]', user.shipping.email, { delay: 100 })
+        await page.type('[id="user.phone"]', user.shipping.phone, { delay: 100 })
+        await page.evaluate(() =>
+          document.querySelectorAll(".button--continue button")[0].click()
+        );
+        await updateDiscord(page, 'Shipping and contact info populated. Heading to payment info!', 'name.png');
+        await page.waitForSelector('[id="optimized-cc-card-number"]', { timeout: 20000 })
+        // fill out payment info and place order
+        await updateDiscord(page, 'Placing order!', 'name.png')
+      }
     }
   } catch (e) {
     await updateDiscord(page, '***** ERROR IN PURCHASE. ABORTING *****', 'error.png')
