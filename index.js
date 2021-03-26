5e797148fdde5504bbd5dda22c6018ae6b8d0fe4
ShepLT1 <luke.t.shepherd@gmail.com>
2021-03-25 21:11:29 -0700

    Added pm2 to handle running index.js in background. Added write of file purchase.json on purchase to avoid repurchase. Added random retry wait times

--- index.js
+++ index.js
@@ -1,151 +1,58 @@
-const puppeteer = require('puppeteer-extra');
-const pluginStealth = require("puppeteer-extra-plugin-stealth");
-puppeteer.use(pluginStealth())
-require('dotenv').config()
-const { Webhook } = require('discord-webhook-node');
-const hook = new Webhook(process.env.DISCORD);
+const pm2 = require('pm2');
+const fs = require('fs');
+const purchasedPath = './purchase.json';
 
-// test: https://www.bestbuy.com/site/pny-geforce-gt1030-2gb-pci-e-3-0-graphics-card-black/5901353.p?skuId=5901353
-// 3070: https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442
+const main = async () => {
 
-const updateDiscord = async (page, message, file) => {
-  try {
-    await page.screenshot({ path: file }, { fullPage: true });
-    console.log(message)
-    hook.send(message);
-    hook.sendFile("./" + file);
-  } catch (e) {
-    throw e
-  }
-}
+    let purchaseCompleted = false
 
-(async () => {
-  let purchased = false;
-  while (purchased === false) {
-    const browser = await puppeteer.launch({
-      ignoreHTTPSErrors: true,
-      // headless: false,
-      product: 'firefox'
-    });
-    const page = await browser.newPage();
-    await page.emulate({
-      name: "Desktop 1024x1200",
-      viewport: {
-        width: 1024,
-        height: 1200,
-      },
-    });
+    const wait = (number) => {
+        return new Promise((resolve) => {
+          setTimeout(resolve, number);
+        });
+    };
+
+    fs.access(purchasedPath, fs.F_OK, (err) => {
+        if (err) {
+            console.log("Purchase.json not found")
+        } else {
+            purchaseCompleted = true
+        }
+    })
 
     try {
-      await page.goto('https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442');
-      await page.waitForSelector('.blue-assist-tab', { timeout: 20000 })
-      const buttonText = await page.$eval('.add-to-cart-button', el => el.innerText);
-      let price = await page.$eval('.priceView-layout-large .priceView-customer-price span', el => el.innerText);
-      price = price.slice(1);
-
-      if (buttonText === "Add to Cart" && price < 650) {
-        await page.evaluate(() =>
-          document.querySelectorAll('.add-to-cart-button')[0].scrollIntoView(false))
-        await page.evaluate(() =>
-          document.querySelectorAll(".add-to-cart-button")[0].click()
-        );
-        await page.waitForTimeout(1000);
-        let clickAgain = false
-        let addedToCart = await page.evaluate(() => document.querySelector('.dot') !== null);
-        while (!clickAgain && !addedToCart) {
-          const cart_btn_available = await page.evaluate(() => document.querySelector('.add-to-cart-button[disabled]') === null);
-          if (cart_btn_available) {
-            console.log("ok to click button")
-            await page.evaluate(() => document.querySelectorAll(".add-to-cart-button")[0].click());
-            clickAgain = true;
-          } else {
-            console.log("waiting")
-            console.log("cart_btn_available", cart_btn_available)
-            await page.waitForTimeout(2000)
-            addedToCart = await page.evaluate(() => document.querySelector('.dot') !== null);
-            console.log("addedToCart", addedToCart)
-          }
+        await wait(5000);
+        if (!purchaseCompleted) {
+            const purchaseProduct = await require('./purchaseProduct');
+            console.log(purchaseProduct)
+        } else if (purchaseCompleted) {
+            console.log("Sleeping for 2 days")
+            await wait(60000 * 60 * 48)
+            process.exit();
         }
-        await updateDiscord(page, 'Added rtx 3070 to cart!', 'added-to-cart.png');
-        await page.waitForTimeout(5000);
-        await page.waitForSelector('.dot', { timeout: 20000 })
-        await page.goto('https://www.bestbuy.com/cart');
-        await page.waitForSelector('.checkout-buttons__checkout', { timeout: 20000 });
-        await page.evaluate(() =>
-          document.querySelectorAll('.change-zipcode-link')[0].click()
-        );
-        await page.waitForSelector('.update-zip__zip-input', { timeout: 20000 })
-        await page.type('.update-zip__zip-input', process.env.ZIP, { delay: 100 })
-        await page.evaluate(() =>
-          document.querySelectorAll('.update-zip__input-group button')[0].click()
-        );
-        await page.waitForTimeout(3000)
-        await page.evaluate(() =>
-          document.querySelectorAll('.checkout-buttons__checkout button:not(disabled)')[0].click()
-        );
-        await updateDiscord(page, 'Proceeding with checkout!', 'checkout.png');
-        await page.waitForSelector('.cia-guest-content', { timeout: 20000 });
-        await page.evaluate(() =>
-          document.querySelectorAll('.cia-guest-content__continue')[0].click()
-        );
-        await updateDiscord(page, 'Continuing as guest!', 'guest.png');
-        await page.waitForSelector('.streamlined__heading', { timeout: 20000 })
-        const headingText = await page.$eval('.streamlined__heading span', el => el.innerText);
-
-        if (headingText === "Store Pickup Information") {
-          console.log("store pickup")
-          await updateDiscord(page, 'Only store pickup available!', 'store-pickup.png');
-        } else {
-          await page.waitForSelector('.address-form__first-name', { timeout: 20000 })
-          await page.type('.address-form__first-name input', process.env.FIRST, { delay: 100 })
-          await page.type('.address-form__last-name input', process.env.LAST, { delay: 100 })
-          await page.evaluate(() =>
-            document.querySelectorAll(".autocomplete__toggle")[0].click()
-          );
-          await page.type('[id="consolidatedAddresses.ui_address_2.street"]', process.env.STREET, { delay: 100 })
-          await page.evaluate(() =>
-            document.querySelectorAll(".address-form__showAddress2Link")[0].click()
-          );
-          await page.waitForSelector('[id="consolidatedAddresses.ui_address_2.street2"]', { timeout: 20000 })
-          await page.type('[id="consolidatedAddresses.ui_address_2.street2"]', process.env.STREET2, { delay: 100 })
-          await page.type('[id="consolidatedAddresses.ui_address_2.city"]', process.env.CITY, { delay: 100 })
-          await page.select('select[name="state"]', process.env.STATE)
-          await page.type('[id="consolidatedAddresses.ui_address_2.zipcode"]', process.env.ZIP, { delay: 100 })
-          await page.type('[id="user.emailAddress"]', process.env.EMAIL, { delay: 100 })
-          await page.type('[id="user.phone"]', process.env.PHONE, { delay: 100 })
-          await page.evaluate(() =>
-            document.querySelectorAll(".button--continue button")[0].click()
-          );
-          await updateDiscord(page, 'Shipping and contact info populated. Heading to payment info!', 'shipping.png');
-          await page.waitForSelector('[id="optimized-cc-card-number"]', { timeout: 20000 })
-          await page.type('[id="optimized-cc-card-number"]', process.env.NUMBER, { delay: 100 });
-          await page.select('select[name="expiration-month"]', process.env.EXPIREMONTH);
-          await page.select('select[name="expiration-year"]', process.env.EXPIREYEAR);
-          await page.type('[id="credit-card-cvv"]', process.env.CVV, { delay: 100 });
-          await updateDiscord(page, 'Placing order!', 'order-preview.png')
-          await page.evaluate(() =>
-            document.querySelectorAll('.button--place-order button')[0].scrollIntoView(false)
-          )
-          // await page.evaluate(() =>
-          //   document.querySelectorAll(".button--place-order button.btn-primary")[0].click()
-          // );
-          await page.waitForSelector('.thank-you-enhancement__emphasis', { timeout: 60000 })
-          await updateDiscord(page, 'HELL YEAH WE GOT IT!', 'order.png')
-          purchased = true;
-          await browser.close();
-        }
-      } else if (buttonText === "Add to Cart" && price >= 650) {
-        await updateDiscord(page, 'Item in stock for ' + price, 'overPrice.png')
-      } else {
-        const time = new Date();
-        console.log("Not in Stock " + time);
-        await page.waitForTimeout(60000)
-        await browser.close();
-      }
+        return true;
     } catch (e) {
-      await updateDiscord(page, '***** ERROR IN PURCHASE. ABORTING *****', 'error.png')
-      await browser.close()
-      throw e
+        throw e
     }
-  }
-})();
+}
+
+pm2.connect(async (e) => {
+    if (e) {
+      console.log(e);
+      process.exit(2);
+    }
+
+    let finished = false;
+  
+    while (!finished) {
+        try {
+            finished = await main();
+          } catch (e) {
+            throw e
+          }
+    }
+        
+    pm2.delete('index', () => {
+      console.log('Process closed');
+    });
+  });
---
+++ purchaseProduct.js
@@ -0,0 +1,168 @@
+const puppeteer = require('puppeteer-extra');
+const pluginStealth = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(pluginStealth())
+require('dotenv').config()
+const fs = require('fs');
+const { Webhook } = require('discord-webhook-node');
+const hook = new Webhook(process.env.DISCORD);
+
+// test: https://www.bestbuy.com/site/pny-nvidia-geforce-gt-710-verto-2gb-ddr3-pci-express-2-0-graphics-card-black/5092306.p?skuId=5092306
+// 3070: https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442
+
+const attemptPurchase = async () => {
+
+  const updateDiscord = async (page, message, file) => {
+    try {
+      await page.screenshot({ path: file }, { fullPage: true });
+      console.log(message)
+      hook.send(message);
+      hook.sendFile("./" + file);
+    } catch (e) {
+      throw e
+    }
+  }
+
+  const getRandomInt = async (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  let purchased = false;
+  while (!purchased) {
+    const browser = await puppeteer.launch({
+      ignoreHTTPSErrors: true,
+      // headless: false,
+      product: 'firefox'
+    });
+    const page = await browser.newPage();
+    await page.emulate({
+      name: "Desktop 1024x1200",
+      viewport: {
+        width: 1024,
+        height: 1200,
+      },
+    });
+
+    try {
+      await page.goto('https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442');
+      const retryWaitTime = await getRandomInt(30000, 60000);
+      await page.waitForSelector('.blue-assist-tab', { timeout: 20000 })
+      const buttonText = await page.$eval('.add-to-cart-button', el => el.innerText);
+      let price = await page.$eval('.priceView-layout-large .priceView-customer-price span', el => el.innerText);
+      price = price.slice(1);
+
+      if (buttonText === "Add to Cart" && price < 650) {
+        await page.evaluate(() =>
+          document.querySelectorAll('.add-to-cart-button')[0].scrollIntoView(false))
+        await page.evaluate(() =>
+          document.querySelectorAll(".add-to-cart-button")[0].click()
+        );
+        await page.waitForTimeout(1000);
+        let clickAgain = false
+        let addedToCart = await page.evaluate(() => document.querySelector('.dot') !== null);
+        while (!clickAgain && !addedToCart) {
+          const cart_btn_available = await page.evaluate(() => document.querySelector('.add-to-cart-button[disabled]') === null);
+          if (cart_btn_available) {
+            console.log("ok to click button")
+            await page.evaluate(() => document.querySelectorAll(".add-to-cart-button")[0].click());
+            clickAgain = true;
+          } else {
+            console.log("waiting")
+            console.log("cart_btn_available", cart_btn_available)
+            await page.waitForTimeout(2000)
+            addedToCart = await page.evaluate(() => document.querySelector('.dot') !== null);
+            console.log("addedToCart", addedToCart)
+          }
+        }
+        await updateDiscord(page, 'Added rtx 3070 to cart!', 'added-to-cart.png');
+        await page.waitForTimeout(5000);
+        await page.waitForSelector('.dot', { timeout: 20000 })
+        await page.goto('https://www.bestbuy.com/cart');
+        await page.waitForSelector('.checkout-buttons__checkout', { timeout: 20000 });
+        await page.evaluate(() =>
+          document.querySelectorAll('.change-zipcode-link')[0].click()
+        );
+        await page.waitForSelector('.update-zip__zip-input', { timeout: 20000 })
+        await page.type('.update-zip__zip-input', process.env.ZIP, { delay: 100 })
+        await page.evaluate(() =>
+          document.querySelectorAll('.update-zip__input-group button')[0].click()
+        );
+        await page.waitForTimeout(3000)
+        await page.evaluate(() =>
+          document.querySelectorAll('.checkout-buttons__checkout button:not(disabled)')[0].click()
+        );
+        await updateDiscord(page, 'Proceeding with checkout!', 'checkout.png');
+        await page.waitForSelector('.cia-guest-content', { timeout: 20000 });
+        await page.evaluate(() =>
+          document.querySelectorAll('.cia-guest-content__continue')[0].click()
+        );
+        await updateDiscord(page, 'Continuing as guest!', 'guest.png');
+        await page.waitForSelector('.streamlined__heading', { timeout: 20000 })
+        const headingText = await page.$eval('.streamlined__heading span', el => el.innerText);
+
+        if (headingText === "Store Pickup Information") {
+          console.log("store pickup")
+          await updateDiscord(page, 'Only store pickup available!', 'store-pickup.png');
+        } else {
+          await page.waitForSelector('.address-form__first-name', { timeout: 20000 })
+          await page.type('.address-form__first-name input', process.env.FIRST, { delay: 100 })
+          await page.type('.address-form__last-name input', process.env.LAST, { delay: 100 })
+          await page.evaluate(() =>
+            document.querySelectorAll(".autocomplete__toggle")[0].click()
+          );
+          await page.type('[id="consolidatedAddresses.ui_address_2.street"]', process.env.STREET, { delay: 100 })
+          await page.evaluate(() =>
+            document.querySelectorAll(".address-form__showAddress2Link")[0].click()
+          );
+          await page.waitForSelector('[id="consolidatedAddresses.ui_address_2.street2"]', { timeout: 20000 })
+          await page.type('[id="consolidatedAddresses.ui_address_2.street2"]', process.env.STREET2, { delay: 100 })
+          await page.type('[id="consolidatedAddresses.ui_address_2.city"]', process.env.CITY, { delay: 100 })
+          await page.select('select[name="state"]', process.env.STATE)
+          await page.type('[id="consolidatedAddresses.ui_address_2.zipcode"]', process.env.ZIP, { delay: 100 })
+          await page.type('[id="user.emailAddress"]', process.env.EMAIL, { delay: 100 })
+          await page.type('[id="user.phone"]', process.env.PHONE, { delay: 100 })
+          await page.evaluate(() =>
+            document.querySelectorAll(".button--continue button")[0].click()
+          );
+          await updateDiscord(page, 'Shipping and contact info populated. Heading to payment info!', 'shipping.png');
+          await page.waitForSelector('[id="optimized-cc-card-number"]', { timeout: 20000 })
+          await page.type('[id="optimized-cc-card-number"]', process.env.NUMBER, { delay: 100 });
+          await page.select('select[name="expiration-month"]', process.env.EXPIREMONTH);
+          await page.select('select[name="expiration-year"]', process.env.EXPIREYEAR);
+          await page.type('[id="credit-card-cvv"]', process.env.CVV, { delay: 100 });
+          await updateDiscord(page, 'Placing order!', 'order-preview.png')
+          await page.evaluate(() =>
+            document.querySelectorAll('.button--place-order button')[0].scrollIntoView(false)
+          )
+          // await page.evaluate(() =>
+          //   document.querySelectorAll(".button--place-order button.btn-primary")[0].click()
+          // );
+          // await page.waitForSelector('.thank-you-enhancement__emphasis', { timeout: 60000 })
+          await updateDiscord(page, 'HELL YEAH WE GOT IT!', 'order.png')
+          fs.writeFileSync('purchase.json', '{}');
+          purchased = true;
+          browser.close();
+          return true
+        }
+      } else if (buttonText === "Add to Cart" && price >= 650) {
+        await updateDiscord(page, 'Item in stock for ' + price, 'overPrice.png')
+        console.log("Retrying in " + retryWaitTime)
+        await page.waitForTimeout(retryWaitTime)
+        await browser.close()
+      } else {
+        const time = new Date();
+        console.log("Not in Stock " + time);
+        console.log("Retrying in " + retryWaitTime)
+        await page.waitForTimeout(retryWaitTime)
+        await browser.close();
+      }
+    } catch (e) {
+      await updateDiscord(page, '***** ERROR IN PURCHASE. ABORTING *****', 'error.png')
+      await browser.close()
+      throw e
+    }
+  }
+};
+
+module.exports = attemptPurchase()
